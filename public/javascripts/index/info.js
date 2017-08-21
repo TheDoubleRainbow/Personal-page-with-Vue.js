2a0225f9c00a8fd6849b56c599cfeaf3d4f6d7fa
TheDoubleRainbow <16mb.com@gmail.com>
2017-08-22 00:23:14 +0300

    s:

--- public/javascripts/index/info.js
+++ public/javascripts/index/info.js
@@ -1,10 +1,19 @@
 Vue.component('info', {
 	template: `
-		<div>
+		<div id="top">
 			<div id="top-info">
-				<img id="top-pic" :src="info.pic"/><br />
-				<span v-show="info.name" id="top-greeting">Hello, my name is <span id="top-name">{{info.name}}</span></span><br />
+				<img v-show="info.pic" id="top-pic" :src="info.pic"/><br />
+				<span v-show="info.name" id="top-greeting">Hello, my name is <span id="top-name">{{info.name}}</span></span>
 				<span id="top-iam">i'm web developer</span>
+			</div>
+			<div id="top-social">
+				<div id="mail">
+					{{info.links.mail}}
+				</div
+				<a :href="info.links.git"><img id="git" class="icon" src="/images/icons/github.png" /></a>
+				<a :href="info.links.linkedin"><img class="icon margin-icon" src="/images/icons/linkedin.png" /></a>
+				<a :href="info.links.facebook"><img class="icon margin-icon" src="/images/icons/facebook.jpg" /></a>
+				<a :href="info.links.twitter"><img class="icon" src="/images/icons/twitter.jpg" /></a>
 			</div>
 		</div>
 	`,
